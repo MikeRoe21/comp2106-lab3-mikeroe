009dbc5b5e17882c126a0ab8f510caa56f5c7eb4
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2018-03-25 13:18:34 -0400

    .

---
+++ config/globals.js
@@ -0,0 +1,10 @@
+// public application level variables
+module.exports = {
+  //'db': 'mongodb://localhost:27017/cartracker'
+    'db': 'mongodb://MikeRoe21:TiggerKitty21@ds221609.mlab.com:21609/mikeroecartracker',
+    'google': {
+        googleClientId: '1016168122082-p54ovs4gb7vlr1pt0g4fsnkc95hht27r.apps.googleusercontent.com',
+        googleClientSecret: 'HuWR_ye51xjfXys09b878CjP',
+        googleCallbackUrl: 'http://localhost:3000/google/callback'
+    }
+};
